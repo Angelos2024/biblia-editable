888ebe979925cf48591f796e1eecd780cce66bd9
Angelos2024 <skyblackroan@gmail.com>
2025-04-11 11:48:10 -0600

    Update drive.js

--- drive.js
+++ drive.js
@@ -110,3 +110,75 @@ function cargarDesdeDrive(nombreArchivo, callback) {
     });
   });
 }
+
+function guardarNotasEnDrive(nombreArchivo, notasJSON) {
+  if (!usuarioGoogle) return;
+
+  inicializarGapi(() => {
+    buscarArchivoExistente(nombreArchivo, (fileId) => {
+      const metadata = {
+        name: nombreArchivo,
+        mimeType: "application/json"
+      };
+
+      const fileContent = new Blob([JSON.stringify(notasJSON, null, 2)], {
+        type: "application/json"
+      });
+
+      const form = new FormData();
+      form.append("metadata", new Blob([JSON.stringify(metadata)], { type: "application/json" }));
+      form.append("file", fileContent);
+
+      const accessToken = gapi.auth.getToken().access_token;
+
+      const url = fileId
+        ? `https://www.googleapis.com/upload/drive/v3/files/${fileId}?uploadType=multipart&fields=id`
+        : "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id";
+
+      fetch(url, {
+        method: fileId ? "PATCH" : "POST",
+        headers: new Headers({ Authorization: "Bearer " + accessToken }),
+        body: form
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log("Notas guardadas en Drive:", data);
+        })
+        .catch(err => {
+          console.error("Error al guardar notas en Drive:", err);
+        });
+    });
+  });
+}
+
+function cargarNotasDesdeDrive(nombreArchivo, callback) {
+  if (!usuarioGoogle) {
+    callback(null);
+    return;
+  }
+
+  inicializarGapi(() => {
+    buscarArchivoExistente(nombreArchivo, (fileId) => {
+      if (!fileId) {
+        callback(null);
+        return;
+      }
+
+      gapi.client.drive.files.get({
+        fileId: fileId,
+        alt: "media"
+      }).then(response => {
+        try {
+          const json = JSON.parse(response.body);
+          callback(json);
+        } catch (e) {
+          console.error("Notas corruptas en Drive", e);
+          callback(null);
+        }
+      }).catch(err => {
+        console.error("Error al cargar notas desde Drive", err);
+        callback(null);
+      });
+    });
+  });
+}
